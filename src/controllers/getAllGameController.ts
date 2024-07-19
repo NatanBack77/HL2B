5ccead4f8eff3d55c1d7bd397f617bdc960618f8
Natanel <natan.devback@gmail.com>
2024-07-19 16:49:54 -0300

    update the code of the library

--- src/controllers/getAllGameController.ts
+++ src/controllers/getAllGameController.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export async function getAllGamesController() {
 	return await axios.get(
-		"https://raw.githubusercontent.com/NatanBack77/HL2B_Data/main/steamGames.json",
+		"https://gist.githubusercontent.com/NatanBack77/d26014d658b46bd33d476f36d606ed24/raw/a87315f5b75ff28af0952e26989b23ef6d78f757/game.json",
 	);
 }
