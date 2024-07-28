e21cd8b8be6fb5a4f5c887ee468bac434ffb7e2d
Natanel <natan.devback@gmail.com>
2024-07-28 12:55:11 -0300

    update the Readme

--- Readme.md
+++ Readme.md
@@ -2,15 +2,34 @@
  
 ```typescript
 
-import { get } from "hl2b"
+import { hl2b } from "../src";
 
-get(game).then((result)=>{
+/**
+ * Essas funções do hl2b são promises, pode ser usadas dentro de uma função assícrona, ou usar o thenCatch pra capturar o response
+ *
+ * @param {string} game param of the game
+ * @param {string} url param of the url
+ * @param {number} limit o limit é um parâmetro opcional do método get, que server pra limitar quantidades de dados do response
+ */
 
-    console.log(result)
+const url =
+	"https://gist.githubusercontent.com/NatanBack77/d26014d658b46bd33d476f36d606ed24/raw/03e3ac5ccf9f738dca4bfabe6198ecea53a0e4c3/game.json";
 
-}).catch((error)=>{
+async function get(url: string, limit?: number) {
+	return await hl2b.get({
+		url,
+		limit,
+	});
+}
 
-    console.log(error)
-})
+async function find(game: string, url: string) {
+	return await hl2b.find(game, url);
+}
+
+// Exemplo de uso:
+get(url);
+find("nameOfTheGame", url);
+
+
 
 ```
--- test/game.ts
+++ test/game.ts
@@ -1,17 +1,27 @@
 import { hl2b } from "../src";
 
-async function get() {
-	console.log(
-		await hl2b.get({
-			url: "https://gist.githubusercontent.com/NatanBack77/d26014d658b46bd33d476f36d606ed24/raw/03e3ac5ccf9f738dca4bfabe6198ecea53a0e4c3/game.json",
-			limit: 1,
-		}),
-	);
-	console.log(
-		await hl2b.find(
-			"Cave Story+",
-			"https://gist.githubusercontent.com/NatanBack77/d26014d658b46bd33d476f36d606ed24/raw/03e3ac5ccf9f738dca4bfabe6198ecea53a0e4c3/game.json",
-		),
-	);
+/**
+ * Essas funções do hl2b são promises, pode ser usadas dentro de uma função assícrona, ou usar o thenCatch pra capturar o response
+ *
+ * @param {string} game param of the game
+ * @param {string} url param of the url
+ * @param {number} limit o limit é um parâmetro opcional do método get, que server pra limitar quantidades de dados do response
+ */
+
+const url =
+	"https://gist.githubusercontent.com/NatanBack77/d26014d658b46bd33d476f36d606ed24/raw/03e3ac5ccf9f738dca4bfabe6198ecea53a0e4c3/game.json";
+
+async function get(url: string, limit?: number) {
+	return await hl2b.get({
+		url,
+		limit,
+	});
 }
-get();
+
+async function find(game: string, url: string) {
+	return await hl2b.find(game, url);
+}
+
+// Exemplo de uso:
+get(url);
+find("nameOfTheGame", url);
